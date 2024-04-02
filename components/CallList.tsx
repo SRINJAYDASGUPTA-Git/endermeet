fd2ce18969d9227eeca84f660f7c345203a30a6b
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-04-02 12:38:29 +0530

    Refactor CallList and MeetingCard components

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,49 +1,72 @@
-"use client"
-import { useGetCalls } from '@/hooks/useGetCalls';
-import { Call, CallRecording } from '@stream-io/video-react-sdk';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
-import MeetingCard from './MeetingCard';
+//@ts-nocheck
 
-const CallList = ({type}:{type:'ended'|'upcoming'|'recordings'}) => {
-    const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls();
-    const [recordings, setRecordings] = useState<CallRecording[]>([])
-    const router = useRouter();
-    const getCalls = ()=>{
-        switch (type) {
-            case 'ended':
-                return endedCalls;
-            case 'upcoming':
-                return upcomingCalls;
-            case 'recordings':
-                return recordings
-            default:
-                return [];
-        }
-    }
-    const getNoCallsMessage = ()=>{
-        switch (type) {
-            case 'ended':
-                return 'No Previous Calls';
-            case 'upcoming':
-                return 'No Upcoming Calls';
-            case 'recordings':
-                return 'No Recordings Available'
-            default:
-                return '';
-        }
-    }
+"use client";
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import MeetingCard from "./MeetingCard";
+import Loader from "./Loader";
 
-    const calls = getCalls();
-    const noCallsMessage = getNoCallsMessage();
+const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+  const router = useRouter();
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "upcoming":
+        return upcomingCalls;
+      case "recordings":
+        return recordings;
+      default:
+        return [];
+    }
+  };
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case "ended":
+        return "No Previous Calls";
+      case "upcoming":
+        return "No Upcoming Calls";
+      case "recordings":
+        return "No Recordings Available";
+      default:
+        return "";
+    }
+  };
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+
+  if(isLoading) return <Loader />
+
   return (
-    <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
-        {calls && calls.length > 0 ? calls.map((meeting:Call|CallRecording) => (
-            <MeetingCard />
-        )): (<div>{noCallsMessage}</div>)
-    }
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? (
+        calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard
+          key={(meeting as Call)?.id}
+            icon={
+                type === "ended"
+                ?'/icons/previous.svg':type === "upcoming"?'/icons/upcoming.svg':'/icons/recordings.svg'
+            }
+            title={(meeting as Call)?.state.custom.description.substring(0, 20) || 'No Description'}
+            date={(meeting as Call)?.state.startsAt?.toLocaleString() || meeting.start_time.toLocaleString()}
+            isPreviousMeeting={type === "ended"}
+            buttonIcon1={type === "recordings" ? '/icons/play.svg' : undefined}
+            handleClick={type === 'recordings'?()=>router.push(`${meeting.url}`):()=>router.push(`/meeting/${meeting.id}`)}
+            link={type === "recordings" ? meeting.url:`${process.env.NEXT_PUBLIC_WEB_URL}/meeting/${meeting.id}`}
+            buttonText={type === "recordings" ? 'Play' : 'Start'}
+          />
+        ))
+      ) : (
+        <div>{noCallsMessage}</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CallList
+export default CallList;
